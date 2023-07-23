a2e73c80f22498c7f057f68e22f897b48b1e2a55
mahesago <marianhsg@gmail.com>
2023-07-23 19:33:48 -0300

    proyecto 2

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+
+//Evento en menú
+const menuItems = document.querySelectorAll('.menu-item');
+menuItems.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    const currentItem = document.querySelector('.active');
+    currentItem.classList.remove('active');
+    e.target.classList.add('active')
+  });
+});
+
+//Menú hamburguesa
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+  nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+  nav.classList.remove("visible");
+})
